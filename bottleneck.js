73e454207c5b18bf94f9972ce4457756ab8504ce
Simon Grondin <github@simongrondin.name>
2018-04-08 13:33:00 -0500

    2.3.0

--- bottleneck.js
+++ bottleneck.js
@@ -1547,7 +1547,7 @@ module.exports={
 },{}],13:[function(require,module,exports){
 module.exports={
   "name": "bottleneck",
-  "version": "2.2.2",
+  "version": "2.3.0",
   "description": "Distributed task scheduler and rate limiter",
   "main": "lib/index.js",
   "typings": "bottleneck.d.ts",
